a1b27d8a54d8b4d93436ec8924b5abc735ddaf40
Viraj-CE <chaklasiyaviraj@gmail.com>
2025-07-18 16:02:06 +0530

    Add files via upload

---
+++ practical 2/practical-2.c
@@ -0,0 +1,14 @@
+//finding the area and the perimeter of the plot
+#include<stdio.h>
+int main()
+{
+    int length=70;
+    int breath=90;
+    int area=length*breath;
+    int x=length+breath;
+    int perimeter=2*x;
+    printf("Area of the plot is \t \t:%d",area);
+    printf("\nperimeter of the plot is \t:%d",perimeter);
+    printf("\nThis Program is Created by \t:Viraj_25TCEJO4");
+    return 0;
+}
