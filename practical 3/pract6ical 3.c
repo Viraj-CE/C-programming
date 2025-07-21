fa6445fc5414059cd0fe9fb186bef724c0eb49e9
Viraj-CE <chaklasiyaviraj@gmail.com>
2025-07-21 16:13:33 +0530

    Add files via upload

--- practical 3/pract6ical 3.c
+++ practical 3/pract6ical 3.c
@@ -9,14 +9,26 @@ int main()
     printf("\nYour Height(in meters)\t :");
     scanf("%f",&Height);
     float BMI=Weight/(Height*Height);
-    printf("Your BMI is \t\t :%f",BMI);
+    printf("Your BMI is \t\t :%.2f",BMI);
     if(BMI<18.5)
     {
         printf("\nYour are Under Weight");
     }
-    else if(BMI>=18.5 | BMI<24.9)
+    else if(BMI>=18.5 & BMI<=24.9)
     {
         printf("\nYou are Healthy");
+    }
+    else if(BMI>24.9 & BMI<=29.9 )
+    {
+        printf("\nYour are OverWeight");
+    }
+    else if(BMI>29.9 & BMI<39.9)
+    {
+        printf("\nYou are Obesity");
+    }
+    else
+    {
+        printf("\nYour are having severe Obesity");
     }
     printf("\nThis is Made MY Viraj Chaklasiya(25TCEJO4)");
     return 0;
