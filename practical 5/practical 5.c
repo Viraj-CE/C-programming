ee600e6bb957de3b3d2b146f7108735d1cd6598c
Viraj-CE <chaklasiyaviraj@gmail.com>
2025-07-25 14:49:15 +0530

    Add files via upload

---
+++ practical 5/practical 5.c
@@ -0,0 +1,14 @@
+//calculating the numbewr of man and women who are illiterate
+#include<stdio.h>
+int main()
+{
+    long long Man=(51.6/100)*1441981744;
+    long long Women=(48.4/100)*1441981744;
+    long long illiterateMan=(19.05/100)*Man;
+    long long illiterateWomen=(37.16/100)*Women;
+    printf("Number Of illitrate Man  :%lld",illiterateMan);
+    printf("\nNumber Of illitrate Women:%lld",illiterateWomen);
+    printf("\nThis is made by \t :Viraj Chakalashiya(25TCEJO4)");
+    printf("\n %lld",Women);
+    return 0;
+}
