85caaf9112cab9ee9d6831ae94775c67c692d2a8
Viraj-CE <chaklasiyaviraj@gmail.com>
2025-08-04 19:20:32 +0530

    Add files via upload

---
+++ practical 6/practical 6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    float C,F;
+    printf("Enter The Temprature(celsius) :");
+    scanf("%f",&C);
+    F=C*(9/5)+32;
+    printf("Your Calculated Convertion is :%.2f",F);
+    printf("\nThis program is developed by : Viraj_25TCEJO4");
+}
