d7f35a3977bea46eb6134a4c0213db1e60ff009a
Viraj-CE <chaklasiyaviraj@gmail.com>
2025-09-22 15:17:01 +0530

    Add files via upload

---
+++ practical 23/practical23.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main()
+{
+    int max=0,profit;
+    int num=0;
+    printf("Enter the Number of Days :");
+    scanf("%d",&num);
+    int price[num];
+    int min=price[0];
+    for(int i=0;i<num;i++)
+    {
+        printf("Enter The Prices On The day %d :",i+1);
+        scanf("%d",&price[i]);
+    }
+    for(int i=0; i<5; i++)
+    {
+        if(price[i]<min)
+        {
+            min=price[i];
+        }
+        else
+        {
+            profit=price[i]-min;
+            if(profit>max)
+            {
+                max=profit;
+            }
+        }
+    }
+    if(max>0)
+    {
+        printf("The profit is: %d",max);
+    }
+    else
+        printf("No profit earned");
+
+}
