98200ccda277392a5ae0a2d290fda7fb64956d15
Viraj-CE <chaklasiyaviraj@gmail.com>
2025-10-17 21:33:50 +0530

    Add files via upload

---
+++ practical 27/Savings_Plans(Fibonacci series)/Savings_Plan.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+     double a=1,b=0,c=1;
+     int n,i=1;
+        printf("----------Savings Plan----------");
+        printf("\nEnter The Number Of Months :");
+        scanf("%d",&n);
+        printf("Month(s)\t\tSavings");
+        if(n>=1)
+        {
+            while(i<=n)
+            {
+                printf("\nMonth %3.d%20.lf",i,c);
+                c=a+b;
+                b=a;
+                a=c;
+                i++;
+            }
+        }
+}
